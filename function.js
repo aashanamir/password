563c016d315be6ccc59047d7e1ab16be7e8b0816
aashanamir <emailstormfacts@gmail.com>
2024-03-23 16:44:57 +0500

    v 0.1

---
+++ function.js
@@ -0,0 +1,39 @@
+// Functions
+function genPassword() {
+
+
+  if (numCheck.checked) {
+    myArr += num;
+  }
+  else {
+    myArr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+  }
+
+  if(charCheck.checked) {
+    myArr += symbols;
+  }
+  else {
+    myArr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+  }
+
+  for (let i = 0; i < passLength; i++) {
+    let pushPassword = Math.floor(Math.random() * myArr.length);
+
+    passwrod += myArr.charAt(pushPassword);
+  }
+
+  passFeild.value = passwrod;
+
+  passwrod = "";
+
+  passChar.innerHTML = passLength;
+}
+
+
+function addSymb() {
+  genPassword();
+}
+
+function addNum() {
+  genPassword();
+}
--- script.js
+++ script.js
@@ -1,72 +1,36 @@
 // Variables
 let passwrod = "";
-  let passLength = 10;
-  let isNum = false;
-  let isSymbol = false;
-  let myArr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-  let num = "1234567890";
-  let symbols = "@#$!%^&*()?><.,/|}{+_=-=~`}";
-
-  // Dom Variables
-  let passChar = document.getElementById("passChar");
-  let passFeild = document.getElementById("passFeild");
-  let charCheck = document.getElementById("charCheck");
-  let numCheck = document.getElementById("numCheck");
-  let btn = document.getElementById("btn");
-  let copyText = passFeild.value;
+let passLength = 8;
+let myArr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+let num = "1234567890";
+let symbols = "@#$!%^&*()?><.,/|}{+_=-=~`}";
 
 
-// Functions
-
-  function genPassword() {
-    if (isNum) myArr += num;
-    if (isSymbol) {
-      myArr += symbols
-    } else {
-      myArr = myArr;
-    }
-    for (let i = 0; i < passLength; i++) {
-      let pushPassword = Math.floor(Math.random() * myArr.length);
-
-      passwrod += myArr.charAt(pushPassword);
-    }
-
-    passFeild.value = passwrod;
-
-    passwrod = "";
-    passChar.innerHTML = passLength;
-  }
+// Dom Variables
+let passChar = document.getElementById("passChar");
+let passFeild = document.getElementById("passFeild");
+let range = document.getElementById("range");
+let charCheck = document.getElementById("charCheck");
+let numCheck = document.getElementById("numCheck");
+let btn = document.getElementById("btn");
+let copyText = passFeild;
 
 
-  function addSymb() {
-    if (charCheck.checked == true) {
-      isSymbol = true;
-    }
-    else {
-      isSymbol = false;
-    }
-    genPassword();
-  }
 
-  function addNum() {
-    isNum = !isNum;
-    genPassword();
-  }
 
-// Calls
+
+// Calling My Function
+genPassword();
+
+
+range.addEventListener("change" , ()=>{
+  passLength = range.value;
   genPassword();
-
-
-
+})
 
 
 
 btn.addEventListener("click", () => {
-    window.navigator.clipboard.writeText(passFeild.value)
-        .then(() => alert("Password copied to clipboard!"))
-        .catch(err => console.error("Error copying text: ", err));
+  window.navigator.clipboard.writeText(passFeild.value)
+  copyText.select()
 });
-
-
-charCheck.addEventListener("change", addSymb);
-numCheck.addEventListener("change", addNum);
